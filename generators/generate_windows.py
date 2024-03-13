4e4236d944cf8c49c63fc6f04234f4b9adb99702
TimB <mexan04@gmail.com>
2024-03-13 21:36:47 +0800

    using subprocess

--- generators/generate_windows.py
+++ generators/generate_windows.py
@@ -9,24 +9,49 @@ import subprocess
 
 from generate_settings import write_customisation, add_resource
 
+WINDOW_PATH = [
+    'window\\second_windows\\about_window',
+    'window\\second_windows\\add_window',
+    'window\\second_windows\\settings\\setting_window',
+    'window\\second_windows\\load_window',
+    'window\\second_windows\\settings\\auto_save_window',
+    'window\\second_windows\\settings\\main_settings',
+    'window\\second_windows\\settings\\user_setting',
+    'window\\second_windows\\authorization_window',
+    'window\\second_windows\\authorization_window\\internal_window\\login',
+    'window\\second_windows\\authorization_window\\internal_window\\registration'
+]
+
+def find_ui_file(path: str):
+    """Находит .ui файл в директории"""
+    files = os.listdir(path)
+    pattern = re.compile(r'.*\.ui')
+    for file in files:
+        if pattern.match(file):
+            return file
+    return None
+
 def generate_class_ui2py(path: str):
     """
     Заглушка
     """
-    files = os.listdir(path)
-    ui_list = []
-    pattern = re.compile(r'.*\.ui')
-    for it in files:
-        if pattern.match(it):
-            ui_list.append(it)
-
-    for it in ui_list:
-        file_path = os.path.join(path, it)
+    ui_file = find_ui_file(path)
+    if ui_file is None:
+        print('В директории файла нет')
+    else:
+        file_path: str = os.path.join(path, find_ui_file(path))
         file_name_without_extension = file_path.split(os.sep)[-1].removesuffix('.ui')
         cmd = f'pyside6-uic {file_path} -o {path}{os.sep}{file_name_without_extension}_class.py'
-        os.popen(cmd)
-        print('Успешная генерация файла')
-        return f'{path}{os.sep}{file_name_without_extension}_class.py'
+        try:
+            subprocess.run(
+                cmd, shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, encoding='utf-8'
+            )
+            print('Успешная генерация файла')
+            return f'{path}{os.sep}{file_name_without_extension}_class.py'
+        except subprocess.CalledProcessError as e:
+            print('Ошибка при генерации файла')
+            raise e
+    return None
 
 def generate_res_rc():
     """
@@ -41,7 +66,7 @@ def generate_res_rc():
         print('Успешная генерация файла ресурсов')
     except subprocess.CalledProcessError as e:
         print('Ошибка при генерации файла ресурсов:')
-        print(e)
+        raise e
 
 def generate_main_window_class():
     """
@@ -74,79 +99,13 @@ def generate_graph_window_class():
                   'def setupUi(self, GraphWindow, user_name):',
                   )
 
-def generate_about_window_class():
-    """
-    Заглушка
-    """
-    path = generate_class_ui2py(f'{os.getcwd()}\\window\\second_windows\\about_window')
-    time.sleep(1)
-    add_resource(path)
-
-def generate_add_window_class():
-    """
-    Заглушка
-    """
-    path = generate_class_ui2py(f'{os.getcwd()}\\window\\second_windows\\add_window')
-    time.sleep(1)
-    add_resource(path)
-
-def generate_setting_window_class():
-    """
-    Заглушка
-    """
-    path = generate_class_ui2py(f'{os.getcwd()}\\window\\second_windows\\settings\\setting_window')
-    time.sleep(1)
-    add_resource(path)
-
-def generate_load_window_class():
-    """
-    Заглушка
-    """
-    path = generate_class_ui2py(f'{os.getcwd()}\\window\\second_windows\\load_window')
-    time.sleep(1)
-    add_resource(path)
-
-def generate_auto_save_window_class():
-    """
-    заглушка
-    """
-    path = generate_class_ui2py(f'{os.getcwd()}\\window\\second_windows\\settings\\auto_save_window')
-    time.sleep(1)
-    add_resource(path)
-
-def generate_settings_window():
-    """
-    заглушка
-    """
-    path = generate_class_ui2py(f'{os.getcwd()}\\window\\second_windows\\settings\\main_settings')
-    time.sleep(1)
-    add_resource(path)
-
-def generate_user_setting_window():
-    """
-    заглушка
-    """
-    path = generate_class_ui2py(f'{os.getcwd()}\\window\\second_windows\\settings\\user_setting')
-    time.sleep(1)
-    add_resource(path)
-
-def generate_authorisation_window():
-    """
-    заглушка
-    """
-    path = generate_class_ui2py(f'{os.getcwd()}\\window\\second_windows\\authorization_window')
-    time.sleep(1)
-    add_resource(path)
-
-def a():
-    path = generate_class_ui2py(f'{os.getcwd()}\\window\\second_windows\\authorization_window\\internal_window\\login')
-    time.sleep(1)
-    add_resource(path)
-
-def b():
-    path = generate_class_ui2py(f'{os.getcwd()}\\window\\second_windows\\authorization_window\\internal_window\\registration')
-    time.sleep(1)
-    add_resource(path)
+def generate_window_wisout_change(paths: list):
+    """делает файлики окон из директорий"""
+    for path in paths:
+        python_file_path =  generate_class_ui2py(path)
+        if python_file_path is not None:
+            time.sleep(1)
+            add_resource(python_file_path)
 
 def genarate_all():
     """
@@ -154,16 +113,7 @@ def genarate_all():
     """
     generate_main_window_class()
     generate_graph_window_class()
-    generate_about_window_class()
-    generate_add_window_class()
-    generate_setting_window_class()
-    generate_load_window_class()
-    generate_auto_save_window_class()
-    generate_settings_window()
-    generate_user_setting_window()
-    generate_authorisation_window()
-    a()
-    b()
+    generate_window_wisout_change(WINDOW_PATH)
     generate_res_rc()
 
 if __name__ == '__main__':
