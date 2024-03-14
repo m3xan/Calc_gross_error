2f566c1f974e4e9ba9b772a8deeec616c196f6c1
TimB <mexan04@gmail.com>
2024-03-14 11:15:40 +0800

    change name

--- window/second_windows/authorization_window/authorization_window.py
+++ window/second_windows/authorization_window/authorization_window.py
@@ -12,24 +12,24 @@ class AuthorizationWindow(QMainWindow):
         self.ui.setupUi(self)
 
         self.aut_win = InternalAutorizationWindow()
-        self.aut_win.registrarion.connect(self.set_AutWindow)
+        self.aut_win.registrarion.connect(self.set_aut_window)
         self.aut_win.fin_aut.connect(self.open_main_window)
 
         self.reg_win = InternalRegistrationWindow()
-        self.reg_win.autorisation.connect(self.set_ReGWindow)
+        self.reg_win.autorisation.connect(self.set_reg_window)
         self.reg_win.reg_finish.connect(self.open_main_window)
 
         self.ui.horizontalLayout.addWidget(self.aut_win)
         self.ui.horizontalLayout.addWidget(self.reg_win)
         self.reg_win.setVisible(False)
 
-    def set_AutWindow(self, signall):
+    def set_aut_window(self, signall):
         if signall:
             self.setWindowTitle('Регистрация')
             self.reg_win.setVisible(True)
             self.aut_win.setVisible(False)
 
-    def set_ReGWindow(self, signall):
+    def set_reg_window(self, signall):
         if signall:
             self.setWindowTitle('Авторизация')
             self.reg_win.setVisible(False)
