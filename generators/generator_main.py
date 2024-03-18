90b943d266fd02d763173c99600a56abb2e61084
TimB <mexan04@gmail.com>
2024-03-18 21:46:22 +0800

    del os

--- generators/generator_main.py
+++ generators/generator_main.py
@@ -1,15 +1,14 @@
 """
 generate Калькулятор.exe
 """
-import os
 
 import PyInstaller.__main__
 
 PyInstaller.__main__.run([
     'main.py',
     '-n=Калькулятор',
-    f'-i={os.getcwd()}/icon/calculator-Freepik.png',
+    '-i=icon/calculator-Freepik.png',
     '-F',
     '-w',
-    f'--version-file={os.getcwd()}/generators/properties.rc'
+    '--version-file=generators/properties.rc'
 ])
