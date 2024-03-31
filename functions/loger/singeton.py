a7e659255de229d95e3adb0bc2bbd1da286cb08b
TimB <mexan04@gmail.com>
2024-03-31 17:54:38 +0800

    12

--- functions/loger/singeton.py
+++ functions/loger/singeton.py
@@ -1,9 +1,10 @@
+from global_param import ECHO
 
 class Singleton(object):
     _instance = None
     _logger = None
     _echo = None
-    def __new__(cls, echo):
+    def __new__(cls, echo = ECHO):
         if not isinstance(cls._instance, cls):
             cls._instance = object.__new__(cls)
             cls._echo = echo
