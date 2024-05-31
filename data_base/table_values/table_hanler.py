7bafadc59c071760ff09ff3db5dccc9a91b9ce34
TimB <mexan04@gmail.com>
2024-05-31 23:39:50 +0800

    refactoring

--- data_base/table_values/table_hanler.py
+++ data_base/table_values/table_hanler.py
@@ -9,18 +9,19 @@ from data_base.table_values.table_model import Dixon_Table
 
 from data_base.engines import table_engine as engine
 
+
 class DatabaseTableHandler:
 
     def __init__(self) -> None:
-        self.__session = Session(engine)
+        self.session = Session(engine)
 
-    def select_romanovsky(self, _n: int, _p: float) -> float | None:
-        with self.__session as session:
+    def select_method(self, method, _n: int, _p: float) -> float | None:
+        with self.session as session:
             smet = select(
-                Romanovsky_Table.value
+                method.value
             ).where(
-                Romanovsky_Table.n_id == _n,
-                Romanovsky_Table.p_id == select(
+                method.n_id == _n,
+                method.p_id == select(
                     P_Value.id
                 ).where(
                     P_Value.p == _p
@@ -30,36 +31,17 @@ class DatabaseTableHandler:
                 return result[0]
             return None
 
+    def select_romanovsky(self, _n: int, _p: float) -> float | None:
+        return self.select_method(
+            Romanovsky_Table, _n, _p
+        )
+
     def select_charlier(self, _n: int, _p: float) -> float | None:
-        with self.__session as session:
-            smet = select(
-                Charlier_Table.value
-            ).where(
-                Charlier_Table.n_id == _n,
-                Charlier_Table.p_id == select(
-                    P_Value.id
-                ).where(
-                    P_Value.p == _p
-                ).scalar_subquery()
-            )
-            result = session.execute(smet).first()
-            if result:
-                return result[0]
-            return None
+        return self.select_method(
+            Charlier_Table, _n, _p
+        )
 
     def select_dixon(self, _n: int, _p: float) -> float | None:
-        with self.__session as session:
-            smet = select(
-                Dixon_Table.value
-            ).where(
-                Dixon_Table.n_id == _n,
-                Dixon_Table.p_id == select(
-                    P_Value.id
-                ).where(
-                    P_Value.p == _p
-                ).scalar_subquery()
-            )
-            result = session.execute(smet).first()
-            if result:
-                return result[0]
-            return None
+        return self.select_method(
+            Dixon_Table, _n, _p
+        )
